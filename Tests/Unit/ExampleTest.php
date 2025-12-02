864573fd241c1c0d471f8b2b666ff5f251304f99
Rémy Perona <remperona@gmail.com>
2025-12-02 11:52:00 -0500

    update unit tests setup and example

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+declare( strict_types=1 );
+
+namespace WPMedia\PackageTemplate\Tests\Unit;
+
+/**
+ * Example test case.
+ */
+class ExampleTest extends TestCase {
+	/**
+	 * Always pass.
+	 */
+	public function testAlwaysPass() {
+		$this->assertTrue( true ); // @phpstan-ignore-line
+	}
+}
